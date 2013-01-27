07c2f5b0d77a1d77f897b844f43e6813b6fedad7
akira <akira@akira-VirtualBox.(none)>
2013-01-27 17:58:24 -0200

    primer commit jajajajaja()

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shunlenon::Application.config.secret_token = '1eb98cafbeff09f90ae201143437f360233d3a89a65fa97cadebe7f3694bf98e1e1852ec4d1741c0f6c0c659f1271a380cb911d31b2a8f7866c6ebf547a32141'
